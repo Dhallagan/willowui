fe044b43cf841e737f39e83f0fadf5f082699479
Dylan <dylanhallagan@msn.com>
2018-05-27 14:04:03 -0400

    Code import (#2)

---
+++ src/components/WillowLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row">
+      <template v-if="sectioned" v-for="slot, name in $slots">
+          <willow-layout-section>
+              <slot :name="name"></slot>
+          </willow-layout-section>
+      </template>
+      <template v-if="!sectioned" v-for="slot, name in $slots">
+          <slot :name="name"></slot>
+      </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "willow-page",
+
+  props: {
+    sectioned: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
